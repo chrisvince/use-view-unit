21cb5e3df9dc2052b113f9b05cbab4343ebe423c
Chris Vince <chrisvince@gmail.com>
2020-07-13 09:01:25 -0400

    Added type checking for window object
    Added type checking for window object for static site renderers such as
    Gatsby. Also updated dependencies. Updated to version 1.0.4.

--- src/useViewUnit/useViewUnit.js
+++ src/useViewUnit/useViewUnit.js
@@ -1,8 +1,11 @@
 import { useEffect, useState, useCallback } from 'react'
 
+const DEFAULT_WINDOW_HEIGHT = typeof window !== 'undefined' && window.innerHeight
+const DEFAULT_WINDOW_WIDTH = typeof window !== 'undefined' && window.innerWidth
+
 const useViewUnit = () => {
-	const [ windowHeight, setWindowHeight ] = useState(window.innerHeight)
-	const [ windowWidth, setWindowWidth ] = useState(window.innerWidth)
+	const [ windowHeight, setWindowHeight ] = useState(DEFAULT_WINDOW_HEIGHT)
+	const [ windowWidth, setWindowWidth ] = useState(DEFAULT_WINDOW_WIDTH)
 
 	const setWindowDimensions = event => {
 		setWindowHeight(event.currentTarget.innerHeight)
@@ -10,8 +13,8 @@ const useViewUnit = () => {
 	}
 
 	useEffect(() => {
-		window.addEventListener('resize', setWindowDimensions)
-		return () => window.removeEventListener('resize', setWindowDimensions)
+		typeof window !== 'undefined' && window.addEventListener('resize', setWindowDimensions)
+		return () => typeof window !== 'undefined' && window.removeEventListener('resize', setWindowDimensions)
 	}, [])
 
 	const vh = useCallback(percent => {
